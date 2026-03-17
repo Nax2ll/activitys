0dac2dc99516a989fe54e885097f5bf6da307bc2
Nax2ll <Naelhimself@gmail.com>
2026-03-17 04:23:42 +0300

    Update User.js

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,45 +1,14 @@
 const mongoose = require('mongoose');
 
-const gameStatSchema = new mongoose.Schema(
-  {
-    played: { type: Number, default: 0 },
-    wins: { type: Number, default: 0 },
-    losses: { type: Number, default: 0 },
-    profit: { type: Number, default: 0 }
-  },
-  { _id: false }
-);
-
-function defaultGameStat() {
-  return {
-    played: 0,
-    wins: 0,
-    losses: 0,
-    profit: 0
-  };
-}
-
 const userSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true, unique: true, index: true },
-    username: { type: String, default: '' },
-    avatar: { type: String, default: '' },
-
-    balance: { type: Number, default: 10000, min: 0 },
-    totalWagered: { type: Number, default: 0 },
-    totalWon: { type: Number, default: 0 },
-
-    stats: {
-      plinko: { type: gameStatSchema, default: defaultGameStat },
-      mines: { type: gameStatSchema, default: defaultGameStat },
-      keno: { type: gameStatSchema, default: defaultGameStat },
-      dice: { type: gameStatSchema, default: defaultGameStat },
-      dragonTower: { type: gameStatSchema, default: defaultGameStat },
-      chickenCross: { type: gameStatSchema, default: defaultGameStat },
-      unknown: { type: gameStatSchema, default: defaultGameStat }
-    }
+    wallet: { type: Number, default: 0 }
   },
-  { timestamps: true }
+  {
+    timestamps: false,
+    collection: 'users'
+  }
 );
 
 module.exports = mongoose.model('User', userSchema);
