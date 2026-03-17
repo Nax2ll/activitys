4a250bc7b39d702b5276ccffa0f3f00b70af016b
Nax2ll <Naelhimself@gmail.com>
2026-03-17 07:17:42 +0300

    Update User.js

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -4,7 +4,13 @@ const userSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true, unique: true, index: true },
     username: { type: String, default: '' },
-    wallet: { type: Number, default: 0 }
+
+    wallet: { type: Number, default: 0 },
+
+    totalWagered: { type: Number, default: 0 },
+    totalWon: { type: Number, default: 0 },
+
+    stats: { type: mongoose.Schema.Types.Mixed, default: {} }
   },
   {
     timestamps: false,
