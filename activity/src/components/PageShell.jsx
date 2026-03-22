f13dc84a7bcac7dab630a124ccc9683b92bbb9ed
Nax2ll <Naelhimself@gmail.com>
2026-03-22 20:12:58 +0300

    Update PageShell.jsx

--- activity/src/components/PageShell.jsx
+++ activity/src/components/PageShell.jsx
@@ -14,7 +14,6 @@ const sidebarItems = [
   { label: 'Guessing Game', path: '/guess' },
     { label: 'Memory Game', path: '/memory' },
   { label: 'Camel Racing', path: '/camel-racing' },
-  { label: 'Guess Who', path: '/guess-who' },
 
 ];
 
